2a5d882e4ea61b42b46a145ec0879f7e75a744c2
Koyel Pramanick <koyelpramanick03@gmail.com>
2026-06-16 21:44:31 +0530

    added another basic functionality, first was user_input will be shoing as user msg and then added user inp will be showing as user_msg and assistant msg both

--- google_colab_notebooks/nitish_singh/genai_notebooks/chatbot_with_ui_and_other_enhancements/streamlit_frontend.py
+++ google_colab_notebooks/nitish_singh/genai_notebooks/chatbot_with_ui_and_other_enhancements/streamlit_frontend.py
@@ -10,3 +10,10 @@ with st.chat_message('user'):
     st.text('My name is Koyel')
 
 user_input = st.chat_input('Type here')
+
+if user_input:
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    with st.chat_message('assistant'):
+        st.text(user_input)
