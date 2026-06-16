f77cff6c8f7e43c036bf0b8a0dabaf4829bc785f
Koyel Pramanick <koyelpramanick03@gmail.com>
2026-06-16 21:57:34 +0530

    added session_history instead of normal list variable so that during each session full chat will be visible

--- google_colab_notebooks/nitish_singh/genai_notebooks/chatbot_with_ui_and_other_enhancements/streamlit_frontend.py
+++ google_colab_notebooks/nitish_singh/genai_notebooks/chatbot_with_ui_and_other_enhancements/streamlit_frontend.py
@@ -1,9 +1,14 @@
 import streamlit as st
 
-message_history = []
+# st.session_state -> dict -> this resets only when user refreshes page, otherwise it will keep accumulating all messages, without it, normal list like below message_history with each user_input full script will be rerun again, with no saving of chat history
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+# message_history = []
 
 # loading the conversation history
-for message in message_history:
+for message in st.session_state['message_history']:
     with st.chat_message(message['role']):
         st.text(message['content'])
 
@@ -21,13 +26,13 @@ user_input = st.chat_input('Type here')
 if user_input:
 
     # first add the new message to message_history
-    message_history.append({'role': 'user', 'content': user_input})
+    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
     with st.chat_message('user'):
         st.text(user_input)
 
 
     # secondly add the new message to message_history
-    message_history.append({'role': 'assistant', 'content': user_input})
+    st.session_state['message_history'].append({'role': 'assistant', 'content': user_input})
     with st.chat_message('assistant'):
         st.text(user_input)
 
