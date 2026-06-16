7b6ae7c810d32d41ccc6529b730abe65aff164ed
Koyel Pramanick <koyelpramanick03@gmail.com>
2026-06-16 21:37:43 +0530

    added basic chat functionality in frontend file

--- google_colab_notebooks/nitish_singh/genai_notebooks/chatbot_with_ui_and_other_enhancements/streamlit_frontend.py
+++ google_colab_notebooks/nitish_singh/genai_notebooks/chatbot_with_ui_and_other_enhancements/streamlit_frontend.py
@@ -1,1 +1,12 @@
 import streamlit as st
+
+with st.chat_message('user'):
+    st.text('hi')
+
+with st.chat_message('assistant'):
+    st.text('How can i help you?')
+
+with st.chat_message('user'):
+    st.text('My name is Koyel')
+
+user_input = st.chat_input('Type here')
