a60e7adcffef1bd618601bfe9e5a81cddf2a4af7
Koyel Pramanick <koyelpramanick03@gmail.com>
2026-06-17 09:29:48 +0530

    added dynamic thread id instead of static thread id

--- google_colab_notebooks/nitish_singh/genai_notebooks/chatbot_with_ui_and_other_enhancements/streamlit_frontend_streaming_threading.py
+++ google_colab_notebooks/nitish_singh/genai_notebooks/chatbot_with_ui_and_other_enhancements/streamlit_frontend_streaming_threading.py
@@ -1,14 +1,24 @@
 import streamlit as st
 from langgraph_backend import chatbot
 from langchain_core.messages import HumanMessage
+import uuid
 
 
-CONFIG = {'configurable': {'thread_id': 'thread-1'}}
+# ************************************************ Utility Functions ********************************
+
+def generate_thread_id():
+    thread_id = uuid.uuid4()
+    return thread_id
 
 
 # ************************************************** Session Setup ***************************************
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
+
+# here if thread id is not generated, this function will generate thread id
+if 'thread_id' not in st.session_state:
+    st.session_state['thread_id'] = generate_thread_id()
+
 
 # ************************************************** Sidebar UI **************************************
 st.sidebar.title('LangGraph Chatbot')
@@ -32,12 +42,14 @@ if user_input:
     st.session_state['message_history'].append({'role': 'user', 'content': user_input})
     with st.chat_message('user'):
         st.text(user_input)
+    
+    CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
 
     with st.chat_message('assistant'):
         ai_message = st.write_stream(
                 message_chunk.content for message_chunk, metadata in chatbot.stream(
                     {'messages': [HumanMessage(content=user_input}]},
-                    config= {'configurable': {'thread_id': 'thread-1'}},
+                    config= CONFIG,
                     stream_mode = ' messsages'
                     )
                 )
